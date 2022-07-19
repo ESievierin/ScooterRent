805de5f80a84dff449e9ea36e7fc4ecf73028159
Esievierin <illia.sievierin@nixsolutions.com>
2022-07-19 17:56:25 +0300

    Added Designer for all DTO,mofify ICustomerService and CustomerService,create PL,added DI and UserContoller to PL

---
+++ ScooterRent.PL/Contollers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScooterRent.BLL.Service;
+using ScooterRent.BLL.Interfaces;
+using ScooterRent.BLL.DTO;
+
+namespace ScooterRent.PL.Contollers
+{
+    public class UserController
+    {
+       public CustomerDTO ActiveCustomer { get; set; }
+       private ICustomerService customerService;
+      
+
+       public UserController(ICustomerService customerservice)
+       {
+            customerService = customerservice;
+       }
+       
+       public void UserSignInOrSingUp()
+       {
+            Console.WriteLine("Choose action :\n1-Sing In\n2-Sing Up");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    SingIn();
+
+                    break;
+                case 2:
+                     SingUp();
+                    break;
+
+            }
+
+        }
+       public void SingUp()
+       {
+            Console.Clear();
+            Console.WriteLine("Sing Up\n");
+            Console.WriteLine("Input you phone number :");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Input you age  :");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            if (customerService.Register(Age,phoneNumber))
+            {
+                SingIn();
+            }
+               
+            
+       }
+      public void SingIn()
+      {
+            Console.Clear();
+            Console.WriteLine("Sing In\n");
+            Console.WriteLine("Input you phone number : ");
+            string phonenumber = Console.ReadLine();
+            var customer = customerService.GetByNumber(phonenumber);
+            if(customer != null)
+            {
+                ActiveCustomer = customer;
+                ShowUserInfo();
+            }
+            else
+            {
+                Console.WriteLine("No user found");
+                Console.WriteLine("Do you want to register?\n1-Yes\n2-No");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        SingUp();
+
+                        break;
+                    case 2:
+                        SingIn();
+                        break;
+
+                }
+            }
+               
+          
+            
+
+        }
+        public void ShowUserInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("Phone number : {0}",ActiveCustomer.PhoneNumber);
+            Console.WriteLine("Age : {0}", ActiveCustomer.Age);
+        }
+    }
+}
+    
+
---
+++ ScooterRent.PL/Program.cs
@@ -0,0 +1,30 @@
+﻿using ScooterRent.BLL.Service;
+using ScooterRent.BLL.Interfaces;
+using ScooterRent.DAL.Interfaces;
+using ScooterRent.DAL.Repositories;
+using ScooterRent.DAL.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ScooterRent.BLL.DTO;
+using ScooterRent.PL.Contollers;
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging()
+    .AddTransient<ICustomerService, CustomerService>()
+    .AddTransient<IScooterService, ScooterService>()
+    .AddTransient<ITarifService, TarifService>()
+    .AddTransient<IRentService, RentServise>()
+    .AddTransient<IWorkUnit, WorkUnit>()
+    .AddTransient<IRepository<Customer>, CustomerRepository>()
+    .AddTransient<IRepository<Scooter>, ScooterRepository>()
+    .AddTransient<IRepository<Tarif>, TarifRepository>()
+    .AddTransient<IRepository<Rent>, RentRepository>()
+    .BuildServiceProvider();
+
+var customerService = serviceProvider.GetService<ICustomerService>();
+UserController userController = new UserController(customerService);
+userController.UserSignInOrSingUp();
+
+
+
+
